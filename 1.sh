06b828307e30160dc2ce311ac6da2902f142f02c
devops <md_syd01@yahoo.co.in>
2020-09-09 22:52:22 +0530

    second commit

--- 1.sh
+++ 1.sh
@@ -7,7 +7,7 @@ echo -n "Enter the file name to Delete: "
   read name
   if rm $name 2>/dev/null
   then
-    echo "\e[1;34mThe given file $name is deleted\e[0m"
+    echo -e "\e[1;34mThe given file $name is deleted\e[0m"
     else
-      echo "\e[1;31mThe given file $name does not exist\e[0m"
+      echo -e "\e[1;31mThe given file $name does not exist\e[0m"
       fi
