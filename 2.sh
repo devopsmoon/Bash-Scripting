76f7800e268a6f777e412dc99887702528dc138a
devops <md_syd01@yahoo.co.in>
2020-09-09 23:19:21 +0530

    my first commit

--- 2.sh
+++ 2.sh
@@ -3,6 +3,6 @@
 #Date: 09-09-2020
 #Description: Mohammed Sayeedudin
 #Modified Date: 09-09-2020
-echo "\e[1;34Today date is $(date +%F)\e[0m"
-echo "\e[1;35The user name is $USER\e[0m"
+echo "\e[1;34mToday date is $(date +%F)\e[0m"
+echo "\e[1;35mThe user name is $USER\e[0m"
 echo "$0"
