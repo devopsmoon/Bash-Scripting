f997a2c55248fa7df2e9057c23d586cc2d523447
devops <md_syd01@yahoo.co.in>
2020-09-09 22:59:28 +0530

    my variable commit

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author: Mohammed sayeed
+#Date: 09-09-2020
+#Description: This script will provide the regular file
+#Modified Date: 09-09-2020
+read -p "Enter your name: " name
+echo -e "\e[1;34mHello Mr $name\e[0m"
+read -p "Enter your Age: " age
+echo -e "\e[1;31mHi your name is $name and your are $age old"
